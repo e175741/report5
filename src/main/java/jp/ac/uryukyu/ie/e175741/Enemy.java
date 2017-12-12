5f05f6ca7752e2e22d0a62ae1c60e3191663c0d4
Kyotaro Yamashiro <e175741@ie.u-ryukyu.ac.jp>
2017-12-12 09:02:38 +0900

    step1

---
+++ src/main/java/jp/ac/uryukyu/ie/e175741/Enemy.java
@@ -0,0 +1,31 @@
+
+package jp.ac.uryukyu.ie.e175741;
+
+public class Enemy extends LivingThing {
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack モンスターの攻撃力
+     */
+    public Enemy (String name, int maximumHP, int attack) {
+        super(name,maximumHP,attack);
+    }
+
+    int HP = getHitPoint();
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    @Override
+    public void wounded(int damage){
+        HP -= damage;
+        if( HP < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+}
